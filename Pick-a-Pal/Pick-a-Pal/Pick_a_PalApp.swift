a359c10cd430f20f549f655242004cb4ba7cecbe
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2024-01-04 18:04:20 +0900

    Create dynamic content(https://developer.apple.com/tutorials/developinswifttutorials/lists-and-textfields)

---
+++ Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by af_nakandakari_toru on 2024/01/04.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
