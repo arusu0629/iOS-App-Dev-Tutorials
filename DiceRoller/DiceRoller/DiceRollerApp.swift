8e14dd4e0c2eeb499ed74ea9dba164010366fa3c
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2024-01-04 17:20:30 +0900

    Update the UI with state(https://developer.apple.com/tutorials/developinswifttutorials/buttons-and-state)

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by af_nakandakari_toru on 2024/01/04.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
