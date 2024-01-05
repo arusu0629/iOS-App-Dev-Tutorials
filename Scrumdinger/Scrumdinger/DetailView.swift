58e65427f661f9f2f37d7621eb1811a96145b6f6
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2024-01-05 15:49:22 +0900

    [Creating a navigation hierarchy Section 2] Create the detail view

---
+++ Scrumdinger/Scrumdinger/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by af_nakandakari_toru on 2024/01/05.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView(scrum: DailyScrum.sampleData[0])
+}
