09615e1b5d9de2332bfd14aef3843e2d2b2bf914
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2024-01-05 12:03:22 +0900

    [Section 2] Compose views

---
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,30 @@
+//
+//  MeetingView.swift
+//  Scrumdinger
+//
+//  Created by af_nakandakari_toru on 2024/01/05.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 10, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MeetingView()
+}
