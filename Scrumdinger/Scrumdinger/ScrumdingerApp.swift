4185b6ad06d63dae3b9c0cbd66742ddb051548d7
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2024-01-05 11:58:00 +0900

    [Section 1] Create a project

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by af_nakandakari_toru on 2024/01/05.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
