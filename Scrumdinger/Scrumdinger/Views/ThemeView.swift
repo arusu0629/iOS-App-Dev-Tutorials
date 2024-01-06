4871fa015b8f666295295209108442f4e1c7fb4f
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2024-01-06 16:55:51 +0900

    [Passing data with bindings Section 1] Add a theme view

---
+++ Scrumdinger/Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,25 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by af_nakandakari_toru on 2024/01/06.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundStyle(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+    ThemeView(theme: .buttercup)
+}
