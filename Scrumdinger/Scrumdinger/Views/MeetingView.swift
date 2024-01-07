dd506da4555ca50c4a839fb7fec58069becedb6d
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2024-01-07 11:05:45 +0900

    [Managing state and life cycle Section 5] Add life cycle events

--- Scrumdinger/Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Scrumdinger/Views/MeetingView.swift
@@ -31,6 +31,13 @@ struct MeetingView: View {
         }
         .padding()
         .foregroundStyle(scrum.theme.accentColor)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
         .navigationBarTitleDisplayMode(.inline)
     }
 }
