df1ddc146949ba6b88ab723b1ebf0293c472b247
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2024-01-08 12:45:14 +0900

    [Updating app data Section 2] Add scrum history

---
+++ Scrumdinger/Scrumdinger/Models/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by af_nakandakari_toru on 2024/01/08.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
+}
